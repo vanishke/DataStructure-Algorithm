eef1e3e881e4485ce7453041d69989c84138724e
vanishke <18685129726@163.com>
2021-04-17 09:55:43 +0800

    直接插入排序

---
+++ src/com/vanishke/cn/sort/DirectInsertSort.java
@@ -0,0 +1,47 @@
+package com.vanishke.cn.sort;
+import java.util.Arrays;
+import static java.lang.System.out;
+// 直接插入排序
+public class DirectInsertSort {
+
+    public static void sort(int arr[])
+    {
+        //记录每次需要直接插入的值
+       int temp;
+       //待排序序列从下表1开始，第一个元素自动有序
+       for(int i=1;i< arr.length;i++)
+       {
+           temp = arr[i];
+           if(arr[i-1] > arr[i])
+           {
+               for(int j=i;j>=0;j--)
+               {
+                   if(j>0 && arr[j-1] > temp)
+                   {
+                        arr[j] = arr[j-1];
+                   }
+                   else
+                   {
+                       arr[j] = temp;
+                       break;
+                   }
+               }
+               out.print("第"+i+"次比较结果");
+               out.println(Arrays.toString(arr));
+           }
+
+       }
+    }
+
+    public static void main(String args[])
+    {
+
+        int arr[] = {2,1,5,3,6,4,9,8,7};
+        out.println("排序前：");
+        out.println(Arrays.toString(arr));
+        sort(arr);
+        out.println("排序后：");
+       out.print(Arrays.toString(arr));
+       out.println();
+    }
+}
